174eaa054bc3fb67756bc1a8aea64ff8ec280cf8
huomozhihen <yq13yq13@sina.com>
2019-07-09 10:35:13 +0800

    steel_demand_year_on_year

--- SteelDemandAnalysis/config/global_config.py
+++ SteelDemandAnalysis/config/global_config.py
@@ -16,5 +16,11 @@ class GlobalConfig:
     machinery_data_path = os.path.join(basic_data_path, '钢铁-需求-机械\\')  # 钢铁-需求-机械
     homeAppAndCar_data_path = os.path.join(basic_data_path, '钢铁-需求-家电汽车\\')  # 钢铁-需求-家电汽车
 
+    steel_importAndExport_data = os.path.join(steel_data_path, '钢材进出口.xlsx')  # 钢铁进出口数据
+    steel_output_data = os.path.join(steel_data_path, '钢材产量.xlsx')  # 钢铁产量数据
+    steel_stock_data = os.path.join(steel_data_path, '钢联数据-社会库存&钢厂库存.xls')  # 钢材库存数据
+    steel_demand_data = os.path.join(steel_data_path, '表观需求.xlsx')  # 钢材需求数据
+    steel_demand_year_on_year_data = os.path.join(steel_data_path, '表观需求同比数据.csv')  # 钢材需求同比数据
+
     def __init__(self):
         pass
--- SteelDemandAnalysis/data_process/steel_demand_data/steel_demand_data_initial.py
+++ SteelDemandAnalysis/data_process/steel_demand_data/steel_demand_data_initial.py
@@ -1,6 +1,41 @@
 """
 Time: 2019-7-5
-Description:
+Description: 进行钢铁需求数据处理
 """
 
+import pandas as pd
+from config import global_config
 
+
+def cal_steel_demand_year_on_year():
+    # 计算钢铁需求同比增长、累计同比增长
+    config = global_config.GlobalConfig()
+
+    steel_demand_data_path = config.steel_demand_data
+    steel_demand_df = pd.read_excel(steel_demand_data_path, usecols=[6, 7])
+    steel_demand_df.index.name = '时间'
+    steel_demand_df.reset_index(inplace=True)
+    steel_demand_array = steel_demand_df['表观需求（万吨）'].values
+    steel_demand_cumulative_array = steel_demand_df['累计表观需求（万吨）'].values
+
+    steel_demand_year_on_year_list = []
+    steel_demand_cumulative_year_on_year_list = []
+    for i in range(len(steel_demand_array)):
+        if i < 12:
+            steel_demand_year_on_year_list.append(0)
+            steel_demand_cumulative_year_on_year_list.append(0)
+        else:
+            steel_demand_year_on_year_list.append(
+                (steel_demand_array[i]-steel_demand_array[i-12])/steel_demand_array[i-12] * 100)
+            steel_demand_cumulative_year_on_year_list.append(
+                (steel_demand_cumulative_array[i] - steel_demand_cumulative_array[i - 12])
+                / steel_demand_cumulative_array[i - 12] * 100)
+    steel_demand_df['同比增长'] = pd.Series(steel_demand_year_on_year_list)
+    steel_demand_df['累计同比增长'] = pd.Series(steel_demand_cumulative_year_on_year_list)
+
+    steel_demand_df.to_csv(config.steel_demand_year_on_year_data,
+                           header=True, index=False, float_format=None)
+
+
+if __name__ == '__main__':
+    cal_steel_demand_year_on_year()
